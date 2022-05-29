db2327300550ffccd8a1432696afd595c732f784
Gabriel Patrick <gabrielpatrickmushi@gmail.com>
2022-05-29 21:15:30 +0300

    add cargo to charges relation

--- app/Models/Cargo.php
+++ app/Models/Cargo.php
@@ -10,4 +10,9 @@ class Cargo extends Model
     use HasFactory;
 
     protected $guarded = ['id'];
+
+    public function charges()
+    {
+        return $this->hasMany(CargoCharge::class);
+    }
 }
